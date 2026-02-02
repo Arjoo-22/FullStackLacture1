b1cb82c095bf7838c620355ba2893c3ce5af1968
arjoo22 <arjoo331@gmail.com>
2026-02-02 14:16:26 +0530

    full stack lect1

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hei");
+console.log("hello world");
+
+const fs=require('fs');
+const data=fs.writeFileSync('example.txt',"This is an example file",
+    function(e,data){
+        if(e) console.log("ye aaya error");
+        console.log(data);
+    }); 
+        
+console.log("File created Successfully");
+console.log("File System operation completed");
+
